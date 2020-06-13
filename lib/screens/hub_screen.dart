35dd9a33a6dd931054974a5cc69e6502c5fd0b1d
Emily Zhu <emilyzhu@berkeley.edu>
2020-06-13 14:37:22 -0700

    add scrolling

--- lib/screens/hub_screen.dart
+++ lib/screens/hub_screen.dart
@@ -404,50 +404,50 @@ class _HubScreenState extends State<HubScreen>{
       ),
       body: SafeArea(
         child: new Container(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                new UserTypeTag(userTag: _userType),
-                new Text(_greeting, style: TextStyle(color: Colors.black, fontSize: 45, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,),
-                Container(
-                  height: MediaQuery.of(context).size.height / 20,
-                  width: MediaQuery.of(context).size.width - 110,
-                  color: Colors.transparent,
-                  child: new AlertTag(message: _message),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width - 110,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
+                      new UserTypeTag(userTag: _userType),
+                      new Text(_greeting, style: TextStyle(color: Colors.black, fontSize: 45, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,),
                       Container(
-                        height: MediaQuery.of(context).size.height / 7,
-                        width: (MediaQuery.of(context).size.width - 120) / 2,
+                        height: MediaQuery.of(context).size.height / 20,
+                        width: MediaQuery.of(context).size.width - 110,
                         color: Colors.transparent,
-                        child: new IncomingItemsCard(incoming: _incoming, onPressed: _onIncomingPressed),
+                        child: new AlertTag(message: _message),
+                      ),
+                      Container(
+                          width: MediaQuery.of(context).size.width - 110,
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Container(
+                                  height: MediaQuery.of(context).size.height / 7,
+                                  width: (MediaQuery.of(context).size.width - 120) / 2,
+                                  color: Colors.transparent,
+                                  child: new IncomingItemsCard(incoming: _incoming, onPressed: _onIncomingPressed),
+                                ),
+                                Container(
+                                  height: MediaQuery.of(context).size.height / 7,
+                                  width: (MediaQuery.of(context).size.width - 120) / 2,
+                                  color: Colors.transparent,
+                                  child: new PendingItemsCard(pending: _pending, onPressed: _onPendingPressed),
+                                ),
+                              ]
+                          )
                       ),
                       Container(
                         height: MediaQuery.of(context).size.height / 7,
-                        width: (MediaQuery.of(context).size.width - 120) / 2,
+                        width: MediaQuery.of(context).size.width - 110,
                         color: Colors.transparent,
-                        child: new PendingItemsCard(pending: _pending, onPressed: _onPendingPressed),
+                        child: new ShippedItemsCard(shipped: _shipped, onPressed: _onShippedPressed),
                       ),
+                      new InventoryButton(onPressed: _onInventoryPressed),
                     ]
-                  )
-                ),
-                Container(
-                  height: MediaQuery.of(context).size.height / 7,
-                  width: MediaQuery.of(context).size.width - 110,
-                  color: Colors.transparent,
-                  child: new ShippedItemsCard(shipped: _shipped, onPressed: _onShippedPressed),
-                ),
-                new InventoryButton(onPressed: _onInventoryPressed),
-              ]
+                )
             )
-          )
-        )
+        ),
       ),
       bottomNavigationBar: new MainBottomNavigationBar(selectedIndex: _selectedIndex, onItemTapped: _onNavigationIconTapped),
     );
@@ -476,58 +476,60 @@ class _HubScreenState extends State<HubScreen>{
         child: new MainAppBar(signOut: signOut),
       ),
       body: SafeArea(
-        child: new Container(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  width: MediaQuery.of(context).size.width - 110,
-                  color: Colors.transparent,
-                  child: new Padding(
-                    child: new Text("Orders", style: TextStyle(color: Colors.black, fontSize: 45, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.left,),
-                    padding: EdgeInsets.only(top: 30, bottom: 25)
+        child: new SingleChildScrollView(
+          child: new Container(
+              child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                          width: MediaQuery.of(context).size.width - 110,
+                          color: Colors.transparent,
+                          child: new Padding(
+                              child: new Text("Orders", style: TextStyle(color: Colors.black, fontSize: 45, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.left,),
+                              padding: EdgeInsets.only(top: 30, bottom: 25)
+                          )
+                      ),
+                      ToggleButtons(
+                        fillColor: Color(0xFFB7CDFF),
+                        borderColor: Color(0xFFC4C4C4),
+                        selectedBorderColor: Color(0xFFC4C4C4),
+                        borderWidth: 2.0,
+                        constraints: BoxConstraints(minWidth: (MediaQuery.of(context).size.width - 110)/3, minHeight: MediaQuery.of(context).size.height / 25),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        children: <Widget>[
+                          new Text("Incoming", style: TextStyle(color: Colors.black, fontSize: 18, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,),
+                          new Text("Pending", style: TextStyle(color: Colors.black, fontSize: 18, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,),
+                          new Text("Shipped", style: TextStyle(color: Colors.black, fontSize: 18, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,),
+                        ],
+                        onPressed: (int index) {
+                          setState(() {
+                            for (int i = 0; i < _isSelectedOrdersPage.length; i++) {
+                              if (i == index) {
+                                _isSelectedOrdersPage[i] = true;
+                              } else {
+                                _isSelectedOrdersPage[i] = false;
+                              }
+                            }
+                            _displayIncoming = _isSelectedOrdersPage[0];
+                            _displayPending = _isSelectedOrdersPage[1];
+                            _displayShipped = _isSelectedOrdersPage[2];
+                          });
+                        },
+                        isSelected: _isSelectedOrdersPage,
+                      ),
+                      if (_displayIncoming) showIncomingItems(),
+                      if (_displayPending) showPendingItems(),
+                      if (_displayShipped) showShippedItems(),
+                    ],
                   )
-                ),
-                ToggleButtons(
-                  fillColor: Color(0xFFB7CDFF),
-                  borderColor: Color(0xFFC4C4C4),
-                  selectedBorderColor: Color(0xFFC4C4C4),
-                  borderWidth: 2.0,
-                  constraints: BoxConstraints(minWidth: (MediaQuery.of(context).size.width - 110)/3, minHeight: MediaQuery.of(context).size.height / 25),
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  children: <Widget>[
-                    new Text("Incoming", style: TextStyle(color: Colors.black, fontSize: 18, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,),
-                    new Text("Pending", style: TextStyle(color: Colors.black, fontSize: 18, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,),
-                    new Text("Shipped", style: TextStyle(color: Colors.black, fontSize: 18, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,),
-                  ],
-                  onPressed: (int index) {
-                    setState(() {
-                      for (int i = 0; i < _isSelectedOrdersPage.length; i++) {
-                        if (i == index) {
-                          _isSelectedOrdersPage[i] = true;
-                        } else {
-                          _isSelectedOrdersPage[i] = false;
-                        }
-                      }
-                      _displayIncoming = _isSelectedOrdersPage[0];
-                      _displayPending = _isSelectedOrdersPage[1];
-                      _displayShipped = _isSelectedOrdersPage[2];
-                    });
-                  },
-                  isSelected: _isSelectedOrdersPage,
-                ),
-                if (_displayIncoming) showIncomingItems(),
-                if (_displayPending) showPendingItems(),
-                if (_displayShipped) showShippedItems(),
-              ],
-            )
+              )
           )
-        )
+        ),
       ),
       bottomNavigationBar: new MainBottomNavigationBar(selectedIndex: _selectedIndex , onItemTapped: _onNavigationIconTapped),
     );
@@ -544,53 +546,55 @@ class _HubScreenState extends State<HubScreen>{
         child: new MainAppBar(signOut: signOut),
       ),
       body: SafeArea(
-        child: new Container(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  width: MediaQuery.of(context).size.width - 110,
-                  color: Colors.transparent,
-                  child: new Padding(
-                    child: new Text(_greeting, style: TextStyle(color: Colors.black, fontSize: 45, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.left,),
-                    padding: EdgeInsets.only(top: 30, bottom: 25)
+        child: new SingleChildScrollView(
+          child: new Container(
+              child: Center(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                            width: MediaQuery.of(context).size.width - 110,
+                            color: Colors.transparent,
+                            child: new Padding(
+                                child: new Text(_greeting, style: TextStyle(color: Colors.black, fontSize: 45, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
+                                  textAlign: TextAlign.left,),
+                                padding: EdgeInsets.only(top: 30, bottom: 25)
+                            )
+                        ),
+                        ToggleButtons(
+                          fillColor: Color(0xFFB7CDFF),
+                          borderColor: Color(0xFFB2B2B2),
+                          selectedBorderColor: Color(0xFFB2B2B2),
+                          borderWidth: 2.0,
+                          constraints: BoxConstraints(minWidth: (MediaQuery.of(context).size.width - 110)/2, minHeight: MediaQuery.of(context).size.height / 25),
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          children: <Widget>[
+                            new Text("Inventory", style: TextStyle(color: Colors.black, fontSize: 18, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.center,),
+                            new Text("Settings", style: TextStyle(color: Colors.black, fontSize: 18, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.center,),
+                          ],
+                          onPressed: (int index) {
+                            setState(() {
+                              _isSelectedProfilePage[index] = true;
+                              _isSelectedProfilePage[1 - index] = false;
+                              _displayInventory = _isSelectedProfilePage[0];
+                              _displaySettings = _isSelectedProfilePage[1];
+                              if (_displayInventory) {
+                                _editButtonPressed = false;
+                              }
+                            });
+                          },
+                          isSelected: _isSelectedProfilePage,
+                        ),
+                        if (_displayInventory) new Text("Inventory", style: TextStyle(color: Colors.black, fontSize: 25, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.left,),
+                        if (_displaySettings) showSettings(),
+                      ]
                   )
-                ),
-                ToggleButtons(
-                  fillColor: Color(0xFFB7CDFF),
-                  borderColor: Color(0xFFB2B2B2),
-                  selectedBorderColor: Color(0xFFB2B2B2),
-                  borderWidth: 2.0,
-                  constraints: BoxConstraints(minWidth: (MediaQuery.of(context).size.width - 110)/2, minHeight: MediaQuery.of(context).size.height / 25),
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  children: <Widget>[
-                    new Text("Inventory", style: TextStyle(color: Colors.black, fontSize: 18, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,),
-                    new Text("Settings", style: TextStyle(color: Colors.black, fontSize: 18, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,),
-                  ],
-                  onPressed: (int index) {
-                    setState(() {
-                      _isSelectedProfilePage[index] = true;
-                      _isSelectedProfilePage[1 - index] = false;
-                      _displayInventory = _isSelectedProfilePage[0];
-                      _displaySettings = _isSelectedProfilePage[1];
-                      if (_displayInventory) {
-                        _editButtonPressed = false;
-                      }
-                    });
-                  },
-                  isSelected: _isSelectedProfilePage,
-                ),
-                if (_displayInventory) new Text("Inventory", style: TextStyle(color: Colors.black, fontSize: 25, fontFamily: 'Roboto', fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.left,),
-                if (_displaySettings) showSettings(),
-              ]
-            )
+              )
           )
-        )
+        ),
       ),
       bottomNavigationBar: new MainBottomNavigationBar(selectedIndex: _selectedIndex, onItemTapped: _onNavigationIconTapped),
     );
